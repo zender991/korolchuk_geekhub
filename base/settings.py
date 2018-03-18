2826def87b583a443e42414d7011dc542a9c4a45
zender991 <zender991@gmail.com>
2018-03-18 16:19:27 +0200

    add user roles

--- base/settings.py
+++ base/settings.py
@@ -88,28 +88,28 @@ WSGI_APPLICATION = 'base.wsgi.application'
 #     }
 # }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'korolchuk_geekhub',
-#          'USER': 'postgres',
-#          'PASSWORD': 'postgres',
-#          'HOST': 'localhost',
-#          'PORT': '',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbunjjsu3o3dq7',
-         'USER': 'blybkztkmgqtkn',
-         'PASSWORD': 'aa2487810d60935f93199fe972f38f11ae52f23a60b5396e47e7187852466cc8',
-         'HOST': 'ec2-54-83-58-222.compute-1.amazonaws.com',
-         'PORT': '5432',
+        'NAME': 'korolchuk_geekhub',
+         'USER': 'postgres',
+         'PASSWORD': 'postgres',
+         'HOST': 'localhost',
+         'PORT': '',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'dbunjjsu3o3dq7',
+#          'USER': 'blybkztkmgqtkn',
+#          'PASSWORD': 'aa2487810d60935f93199fe972f38f11ae52f23a60b5396e47e7187852466cc8',
+#          'HOST': 'ec2-54-83-58-222.compute-1.amazonaws.com',
+#          'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
