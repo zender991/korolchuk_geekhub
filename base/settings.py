1008e768fcae32141a24053b8b7443d97cabbe2d
zender991 <zender991@gmail.com>
2018-04-01 16:42:16 +0300

    update cron

--- base/settings.py
+++ base/settings.py
@@ -166,14 +166,14 @@ EMAIL_HOST_PASSWORD = password
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-# STATIC_URL = '/static/'
-#
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-#
-# # STATICFILES_DIRS = [
-# #     os.path.join(BASE_DIR, "static"),
-# #     ]
-#
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    ]
+
 # STATICFILES_DIRS = ()
 
 MEDIA_URL = "/media/"
