dce6df06c8b08ad576b07488e10e5be9c1991383
zender991 <zender991@gmail.com>
2018-04-01 16:48:11 +0300

    add static

--- base/settings.py
+++ base/settings.py
@@ -170,11 +170,11 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    ]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     ]
 
-# STATICFILES_DIRS = ()
+STATICFILES_DIRS = ()
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
